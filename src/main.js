8197f147207d0f7db56d2f0c653fdc47221a0bd6
heren <heren1heren@gmail.com>
2023-12-06 12:02:22 -0700

    aoeua

--- src/main.js
+++ src/main.js
@@ -24,40 +24,38 @@ const nextBtn = document.querySelector('#right-btn');
 const prevBtn = document.querySelector('#left-btn');
 const imageContainer = document.querySelector('.image-container');
 
+window.addEventListener('DOMContentLoaded', () => {
+  renderImage(count)
+});
 nextBtn.addEventListener('click', () => {
   count++;
   if (count > imageLength - 1) {
     count = 0;
   }
-  const imageText = returnImageStructure(count);
-  injectHtml(imageText, imageContainer);
-
+  renderImage(count);
 });
 
 prevBtn.addEventListener('click', () => {
   count--;
   if (count < 0) {
     count = imageLength - 1;
-    console.log(count);
   }
-  const imageText = returnImageStructure(count);
-  injectHtml(imageText, imageContainer);
-
+  renderImage(count);
 });
-const injectHtml = function (htmlText, container) {
-  console.log(container.innerHTML);
-  clear(container);
-  container.innerHtml = htmlText;
-  console.log(container.innerHTML);
 
-};
-
-const returnImageStructure = function (count) {
-  return `<img src="${images.at(count).src}" alt="">`;
-};
-
-function clear(container) {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
+function renderImage(count) {
+  const imageText = returnImageStructure(count);
+  imageContainer.innerHTML = imageText;
+  const image = document.querySelector('[data-image]');
+  setTimeout(() => {
+    image.classList.add('img-transition');
+  }, 200);
 }
+
+function returnImageStructure(count) {
+  return `<img src="${images.at(count).src}" alt="" data-image>`;
+}
+setInterval(() => {
+  const randomCount = Math.round(1 + Math.random()*4);
+  renderImage(randomCount)
+}, 5000);
